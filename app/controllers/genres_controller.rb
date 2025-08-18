7b1e155fb3114df296d9ab6be3abbeb27d6542a3
Emily Kanarek <emilyakanarek@gmail.com>
2025-08-18 15:20:21 -0400

    Create a new genre

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -3,5 +3,20 @@ class GenresController < ApplicationController
   def show 
     @genre = Genre.find(params[:id])
   end
-  
+
+  def new 
+    @genre = Genre.new 
+  end
+
+  def create 
+    @genre = Genre.create(genre_params)
+    redirect_to genre_path(@genre)
+  end
+
+  private 
+
+  def genre_params 
+    params.require(:genre).permit(:name)
+  end
+
 end
