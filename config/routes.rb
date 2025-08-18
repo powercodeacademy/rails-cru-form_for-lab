91ef1d64315ffa3bb3e23966504f496619bb481a
Emily Kanarek <emilyakanarek@gmail.com>
2025-08-18 15:36:38 -0400

    Edit a song

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :songs, only: [:show, :new, :create]
+  resources :songs, only: [:show, :new, :create, :edit, :update]
   resources :genres, only: [:show, :new, :create, :edit, :update]
   resources :artists, only: [:show, :new, :create, :edit, :update]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
