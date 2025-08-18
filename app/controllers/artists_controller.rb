c2a66bbad2de3cb75b801e03a99cf985bfa68cd9
Emily Kanarek <emilyakanarek@gmail.com>
2025-08-18 15:15:59 -0400

    Edit an artist

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -12,6 +12,16 @@ class ArtistsController < ApplicationController
     redirect_to artist_path(@artist)
   end
 
+  def edit 
+    @artist = Artist.find(params[:id])
+  end
+
+  def update 
+    @artist = Artist.find(params[:id])
+    @artist.update(artist_params)
+    redirect_to artist_path(@artist)
+  end
+
   private 
 
   def artist_params 
