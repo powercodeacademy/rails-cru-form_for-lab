fa43c0ff96fd7aacf41c76a971527df61615d2c9
Emily Kanarek <emilyakanarek@gmail.com>
2025-08-18 15:13:28 -0400

    Add a new artist

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -2,4 +2,19 @@ class ArtistsController < ApplicationController
   def show 
     @artist = Artist.find(params[:id])
   end
+
+  def new 
+    @artist = Artist.new 
+  end
+
+  def create 
+    @artist = Artist.create(artist_params)
+    redirect_to artist_path(@artist)
+  end
+
+  private 
+
+  def artist_params 
+    params.require(:artist).permit(:name, :bio)
+  end
 end
