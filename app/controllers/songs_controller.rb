ed9bc41c29ef6919fbc51c62a89788dbcd3a3f7e
Nihar Patel <niharpatel52@gmail.com>
2025-08-18 14:08:09 -0400

    Create CRU forms for Genre Class

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,70 +1,35 @@
-class SongsController < ApplicationController
-  before_action :set_song, only: %i[ show edit update destroy ]
-
-  # GET /songs or /songs.json
+class ArtistsController < ApplicationController
   def index
-    @songs = Song.all
+    @artists = Artist.all
   end
 
-  # GET /songs/1 or /songs/1.json
   def show
+    @artist = Artist.find(params[:id])
   end
 
-  # GET /songs/new
   def new
-    @song = Song.new
+    @artist = Artist.new
   end
 
-  # GET /songs/1/edit
-  def edit
-  end
-
-  # POST /songs or /songs.json
   def create
-    @song = Song.new(song_params)
-
-    respond_to do |format|
-      if @song.save
-        format.html { redirect_to @song, notice: "Song was successfully created." }
-        format.json { render :show, status: :created, location: @song }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @song.errors, status: :unprocessable_entity }
-      end
-    end
+    @artist = Artist.new(artist_params)
+    @artist.save
+    redirect_to @artist
   end
 
-  # PATCH/PUT /songs/1 or /songs/1.json
+  def edit
+    @artist = Artist.find(params[:id])
+  end
+
   def update
-    respond_to do |format|
-      if @song.update(song_params)
-        format.html { redirect_to @song, notice: "Song was successfully updated." }
-        format.json { render :show, status: :ok, location: @song }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @song.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /songs/1 or /songs/1.json
-  def destroy
-    @song.destroy
-
-    respond_to do |format|
-      format.html { redirect_to songs_path, status: :see_other, notice: "Song was successfully destroyed." }
-      format.json { head :no_content }
-    end
+    @artist = Artist.find(params[:id])
+    @artist.update(artist_params)
+    redirect_to @artist
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_song
-      @song = Song.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def song_params
-      params.require(:song).permit(:name, :artist_id, :genre_id)
-    end
+  def artist_params
+    params.require(:artist).permit(:name, :bio)
+  end
 end
