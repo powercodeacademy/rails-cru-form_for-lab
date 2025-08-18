3107a72cb6946e781bdd4f26d6880b34f9131528
Emily Kanarek <emilyakanarek@gmail.com>
2025-08-18 15:18:10 -0400

    Show genre info

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :songs
-  resources :genres
+  resources :genres, only: [:show]
   resources :artists, only: [:show, :new, :create, :edit, :update]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
