f118d23ad02507ac120183b841a68ad889349b5a
Emily Kanarek <emilyakanarek@gmail.com>
2025-08-18 15:21:34 -0400

    Edit a genre

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -13,6 +13,16 @@ class GenresController < ApplicationController
     redirect_to genre_path(@genre)
   end
 
+  def edit 
+    @genre = Genre.find(params[:id])
+  end
+
+  def update 
+    @genre = Genre.find(params[:id])
+    @genre.update(genre_params)
+    redirect_to genre_path(@genre)
+  end
+
   private 
 
   def genre_params 
