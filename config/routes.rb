b0e515e0ae39604b3c366397dc236452eec92db4
Jen Kelly <jen.kelly@powerhrg.com>
2025-08-19 10:53:06 -0400

    Create songs class and table. Add associations

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :songs, only: %i[index show new create updated_at]
   resources :genres, only: %i[show new create edit update]
   resources :artists, only: %i[show new create edit update]
 end
