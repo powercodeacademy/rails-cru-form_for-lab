e65b0fea6ce7e6d4eba4d00e09a2a089fba5fe0d
Jen Kelly <jen.kelly@powerhrg.com>
2025-08-19 11:05:06 -0400

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,11 @@
-Artist.create(name: 'Brittany Spears', bio: 'Whoops! She did it again.')
-Artist.create(name: 'Freddie Mercury', bio: 'Front man for Queen.')
-Artist.create(name: 'Kurt Cobain', bio: 'Front man for Nirvana.')
+brittany = Artist.create(name: 'Brittany Spears', bio: 'Whoops! She did it again.')
+freddie = Artist.create(name: 'Freddie Mercury', bio: 'Front man for Queen.')
+kurt = Artist.create(name: 'Kurt Cobain', bio: 'Front man for Nirvana.')
 
-Genre.create(name: 'Pop')
-Genre.create(name: 'Rock')
-Genre.create(name: 'Grunge')
+pop = Genre.create(name: 'Pop')
+rock = Genre.create(name: 'Rock')
+grunge = Genre.create(name: 'Grunge')
+
+Song.create(name: 'Smells Like Teen Spirit', artist_id: kurt.id, genre_id: grunge.id)
+Song.create(name: 'We Will Rock You', artist_id: freddie.id, genre_id: rock.id)
+Song.create(name: 'Whoops! I Did it Again', artist_id: brittany.id, genre_id: pop.id)
