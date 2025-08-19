a74197e7b0f9f00b747af84961964e5dadf36cb5
Jen Kelly <jen.kelly@powerhrg.com>
2025-08-19 10:09:53 -0400

    Write genre show action. Add route

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -1,2 +1,21 @@
 class GenresController < ApplicationController
+  def show
+    @genre = Genre.find(params[:id])
+  end
+
+  def new
+    @genre = Genre.new
+  end
+
+  def create
+    @genre = Genre.create(genre_params)
+    @genre.save
+    redirect_to genre_path
+  end
+
+  private
+
+  def genre_params
+    params.require(:genre).permit(:name)
+  end
 end
