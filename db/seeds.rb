dea9897eebc2ddcc0de8f2f6294b7cf9b6110b1b
Jen Kelly <jen.kelly@powerhrg.com>
2025-08-19 09:08:38 -0400

    Create and mmigrate artist table. Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Artist.create(name: 'Vincent Van Gogh', bio: 'He cut off his ear.')
+Artist.create(name: 'Frida Kahlo', bio: 'She had weird eyebrows.')
+Artist.create(name: 'Claude Monet', bio: 'Really liked water lillies.')
