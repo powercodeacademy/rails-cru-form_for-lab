1c27ca68807bff732651333846f9cb2352b1a087
Jen Kelly <jen.kelly@powerhrg.com>
2025-08-19 09:22:18 -0400

    Show new artost form that submits content and displays on artist page

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -6,4 +6,20 @@ class ArtistsController < ApplicationController
   def show
     @artist = Artist.find(params[:id])
   end
+
+  def new
+    @artist = Artist.new
+  end
+
+  def create
+    @artist = Artist.create(artist_params)
+    @artist.save
+    redirect_to artist_path(@artist)
+  end
+
+  private
+
+  def artist_params
+    params.require(:artist).permit(:name, :bio)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,3 @@
 Rails.application.routes.draw do
-  resources :artists
+  resources :artists, only: %i[index show new create]
 end
