b27236d595f16378039cfe0972039314090c64f1
Jen Kelly <jen.kelly@powerhrg.com>
2025-08-19 11:10:40 -0400

    Write song actions index, show, new, create, edit, update and helpers set_song and song_params

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,2 +1,36 @@
 class SongsController < ApplicationController
+  before_action :set_song, only: %i[show edit update]
+
+  def index
+    @songs = Song.all
+  end
+
+  def show; end
+
+  def new
+    @song = Song.new
+  end
+
+  def create
+    @song = Song.create(song_params)
+    @song.save
+    redirect_to song_path(@song)
+  end
+
+  def edit; end
+
+  def update
+    @song.update(song_params)
+    redirect_to song_path(@song)
+  end
+
+  private
+
+  def set_song
+    @song = Song.find(params[:id])
+  end
+
+  def song_params
+    params.require(:song).permit(:name)
+  end
 end
