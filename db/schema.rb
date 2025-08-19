ef15508b262c0ca5882ad585460b01d0eb64fac4
Jen Kelly <jen.kelly@powerhrg.com>
2025-08-19 09:13:17 -0400

    Show artist anme and bio on show page

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2025_08_18_202528) do
+
+  create_table "artists", force: :cascade do |t|
+    t.string "name"
+    t.string "bio"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end
